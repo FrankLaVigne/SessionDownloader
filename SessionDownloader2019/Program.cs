8529b730335e452dda01d67802f7fcb2737bb52c
Frank La Vigne <frankla@microsoft.com>
2019-06-20 17:30:14 -0400

    Console UI enhancements

--- SessionDownloader2019/Program.cs
+++ SessionDownloader2019/Program.cs
@@ -12,15 +12,26 @@ namespace SessionDownloader2019
     {
         private static char[] invalidFilenameChars = { '\\', '/', ':', '*', '?', '"', '<', '>', '|', '\n', '.' };
 
+        private static ConsoleColor defaultForegroundConsoleColor = Console.ForegroundColor;
+        private static ConsoleColor defaultBackgroundConsoleColor = Console.BackgroundColor;
+
+        enum MessageLevel
+        {
+            Normal,
+            Highlight,
+            Warning,
+            Error
+        }
+
         public class Arguments
         {
             public string DestinationPath { get; set; }
         }
 
         static Arguments arguments;
-
         static void Main(string[] args)
         {
+
             arguments = ParseArgs(args);
 
             if (arguments == null)
@@ -28,35 +39,36 @@ namespace SessionDownloader2019
                 return;
             }
 
-            WebClient webClient = new WebClient();
-            string sourceData = "https://api.mybuild.techcommunity.microsoft.com/api/session/all";
-
-            Console.WriteLine("Starting Download");
-            string allThatJson = webClient.DownloadString(sourceData);
-            Console.WriteLine("Download Complete");
+            string allThatJson = DownloadSessionMetaData();
 
             dynamic sessions = JArray.Parse(allThatJson);
 
             Console.WriteLine($"{sessions.Count} talks found.");
 
-            DownloadSessions(sessions);
+            //DownloadSessions(sessions);
 
-
-
-
-            Console.WriteLine("Finished!");
-
-
+            Console.WriteLine($"Finished at {DateTime.Now}");
 
             Console.ReadLine();
 
+        }
+
+        private static string DownloadSessionMetaData()
+        {
+            WebClient webClient = new WebClient();
+            string sourceData = "https://api.mybuild.techcommunity.microsoft.com/api/session/all";
+
+            WriteHighlight("Starting Metadata Download");
+            string allThatJson = webClient.DownloadString(sourceData);
+            WriteHighlight("Metadata Download Complete");
+            return allThatJson;
         }
 
         private static Arguments ParseArgs(string[] args)
         {
             if (args.Length < 1)
             {
-                Console.WriteLine("Please enter a destination path!");
+                WriteError("Please enter a destination path!");
                 return null;
             }
             var destinationPath = args[0];
@@ -86,13 +98,20 @@ namespace SessionDownloader2019
 
                     if (File.Exists(destinationFilename) == true)
                     {
-                        Console.WriteLine("File exists. Skipping");
+                        WriteWarning("File exists. Skipping");
                     }
                     else
                     {
-                        DownloadFile(remoteUri, destinationFilename);
-                    }
+                        try
+                        {
+                            DownloadFile(remoteUri, destinationFilename);
+                        }
+                        catch (Exception exception)
+                        {
+                            WriteError($"Error downloading {remoteUri} to {destinationFilename}");
+                        }
 
+                    }
 
                     Console.WriteLine($"{destinationFilename}");
                 }
@@ -115,11 +134,7 @@ namespace SessionDownloader2019
                         DownloadFile(remoteUri, destinationFilename);
                     }
                 }
-
-
-
             }
-
         }
 
         private static void DownloadFile(string remoteUri, string destinationFilename)
@@ -145,5 +160,70 @@ namespace SessionDownloader2019
 
             return scrubbedString;
         }
+
+        private static void ResetConsoleColors()
+        {
+            Console.ForegroundColor = defaultForegroundConsoleColor;
+            Console.BackgroundColor = defaultBackgroundConsoleColor;
+        }
+
+        private static void WriteError(string message)
+        {
+            WriteMessage(message, MessageLevel.Error);
+        }
+        private static void WriteWarning(string message)
+        {
+            WriteMessage(message, MessageLevel.Warning);
+        }
+        private static void WriteHighlight(string message)
+        {
+            WriteMessage(message, MessageLevel.Highlight);
+        }
+
+
+        private static void WriteMessage(string message, MessageLevel messageLevel)
+        {
+            switch (messageLevel)
+            {
+                case MessageLevel.Normal:
+                    break;
+                case MessageLevel.Highlight:
+                    InvertConsoleColors();
+                    break;
+                case MessageLevel.Warning:
+                    WarningConsoleColors();
+                    break;
+                case MessageLevel.Error:
+                    ErrorConsoleColors();
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine(message);
+
+            ResetConsoleColors();
+
+
+        }
+
+        private static void InvertConsoleColors()
+        {
+            Console.ForegroundColor = defaultBackgroundConsoleColor;
+            Console.BackgroundColor = defaultForegroundConsoleColor;
+        }
+
+        private static void ErrorConsoleColors()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = defaultBackgroundConsoleColor;
+        }
+
+        private static void WarningConsoleColors()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = defaultBackgroundConsoleColor;
+        }
+
     }
 }
